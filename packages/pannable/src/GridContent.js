d6d00a28ecfbad11ed7ecb4b18b735435b9c5465
朱德明 <jamingming@163.com>
2019-03-11 16:07:38 +0800

    no message

--- packages/pannable/src/GridContent.js
+++ packages/pannable/src/GridContent.js
@@ -2,124 +2,142 @@ import React from 'react';
 
 export default class GridContent extends React.PureComponent {
   static defaultProps = {
-    rowCount: 0,
-    columnCount: 0,
-    rowHeight: 0,
-    columnWidth: 0,
-    rowHeightHash: ({ rowIndex }) => '' + rowIndex,
-    columnWidthHash: ({ columnIndex }) => '' + columnIndex,
-    cellKey: ({ columnIndex, rowIndex }) => rowIndex + '-' + columnIndex,
-    renderCell: () => null,
+    direction: 'vertical',
+    width: -1,
+    height: -1,
+    itemWidth: 0,
+    itemHeight: 0,
+    itemKey: ({ columnIndex, rowIndex }) => rowIndex + '-' + columnIndex,
+    renderItem: () => null,
+    count: 0,
     visibleRect: { x: 0, y: 0, width: 0, height: 0 },
+    lineSpacing: 0,
+    inneritemSpacing: 0,
     onResize: () => {},
   };
 
   state = {
-    size: { width: 0, height: 0 },
-    widthDict: {},
-    heightDict: {},
-    xList: [],
-    yList: [],
+    rowCount: 0,
+    columnCount: 0,
+    sizeWidth: 0,
+    sizeHeight: 0,
+    layoutAttrs: [],
   };
 
-  static getDerivedStateFromProps(props, state) {
-    const {
-      columnWidth,
-      rowHeight,
-      columnCount,
-      rowCount,
-      rowHeightHash,
-      columnWidthHash,
-      onResize,
-    } = props;
-    const { xList, yList, widthDict, heightDict, size } = state;
-    let nextState = {};
-    let nextSize = { ...size };
-
-    const rowState = calculateDerivedState(
-      rowCount,
-      rowHeight,
-      rowHeightHash,
-      yList,
-      heightDict
-    );
-    if (rowState.originList) {
-      nextState.yList = rowState.originList;
-    }
-    if (rowState.sizeDict) {
-      nextState.heightDict = rowState.sizeDict;
-    }
-    nextSize.height = rowState.size;
-
-    const columnState = calculateDerivedState(
-      columnCount,
-      columnWidth,
-      columnWidthHash,
-      xList,
-      widthDict
-    );
-    if (columnState.originList) {
-      nextState.xList = columnState.originList;
-    }
-    if (columnState.sizeDict) {
-      nextState.widthDict = columnState.sizeDict;
-    }
-    nextSize.width = columnState.size;
-
-    if (nextSize.width !== size.width || nextSize.height !== size.height) {
-      onResize(nextSize);
-      nextState.size = nextSize;
-    }
-
-    return nextState;
+  componentDidMount() {
+    this._calculateLayout();
   }
 
-  getCellRect({ rowIndex, columnIndex }) {
-    const { xList, yList } = this.state;
-    const x = xList[columnIndex - 1] || 0;
-    const y = yList[rowIndex - 1] || 0;
-    const x2 = xList[columnIndex] || 0;
-    const y2 = yList[rowIndex] || 0;
+  componentDidUpdate(prevProps) {
+    const {
+      width,
+      height,
+      itemWidth,
+      itemHeight,
+      lineSpacing,
+      inneritemSpacing,
+      count,
+      direction,
+    } = this.props;
 
-    return { x, y, width: x2 - x, height: y2 - y };
+    if (
+      prevProps.width !== width ||
+      prevProps.height !== height ||
+      prevProps.itemWidth !== itemWidth ||
+      prevProps.itemHeight !== itemHeight ||
+      prevProps.lineSpacing !== lineSpacing ||
+      prevProps.inneritemSpacing !== inneritemSpacing ||
+      prevProps.count !== count ||
+      prevProps.direction !== direction
+    ) {
+      this._calculateLayout();
+    }
+  }
+
+  _calculateLayout() {
+    this.setState((state, props) => {
+      const {
+        width,
+        height,
+        itemWidth,
+        itemHeight,
+        lineSpacing,
+        inneritemSpacing,
+        count,
+        direction,
+        onResize,
+      } = props;
+
+      const nextState = calculateLayout(
+        { width, height },
+        { width: itemWidth, height: itemHeight },
+        lineSpacing,
+        inneritemSpacing,
+        count,
+        direction
+      );
+
+      if (
+        state.sizeWidth !== nextState.sizeWidth ||
+        state.sizeHeight !== nextState.sizeHeight
+      ) {
+        onResize({ width: nextState.sizeWidth, height: nextState.sizeHeight });
+      }
+
+      return nextState;
+    });
+  }
+
+  getItemRect({ rowIndex, columnIndex }) {
+    const { direction } = this.props;
+    const { rowCount, columnCount, layoutAttrs } = this.state;
+    const index = calculateIndexForLayoutAttrs(
+      { row: rowIndex, column: columnIndex },
+      { row: rowCount, column: columnCount },
+      direction
+    );
+
+    if (index < layoutAttrs.length) {
+      const attrs = layoutAttrs[index];
+
+      return {
+        x: attrs.x,
+        y: attrs.y,
+        width: attrs.width,
+        height: attrs.height,
+      };
+    }
+
+    return null;
   }
 
   render() {
-    const { visibleRect, renderCell, cellKey, children } = this.props;
+    const { visibleRect, renderItem, itemKey, children } = this.props;
+    const { layoutAttrs } = this.state;
 
     if (typeof children === 'function') {
       return children(this);
     }
 
-    const { xList, yList } = this.state;
     const grids = [];
 
-    for (let rowIndex = 0; rowIndex < yList.length; rowIndex++) {
-      for (let columnIndex = 0; columnIndex < xList.length; columnIndex++) {
-        const cellRect = this.getCellRect({ rowIndex, columnIndex });
+    for (let index = 0; index < layoutAttrs.length; index++) {
+      const attrs = layoutAttrs[index];
 
-        if (
-          needsRender(
-            { x: cellRect.x, y: cellRect.y },
-            { width: cellRect.width, height: cellRect.height },
-            { x: visibleRect.x, y: visibleRect.y },
-            { width: visibleRect.width, height: visibleRect.height }
-          )
-        ) {
-          const cellStyle = {
-            position: 'absolute',
-            left: cellRect.x,
-            top: cellRect.y,
-            width: cellRect.width,
-            height: cellRect.width,
-          };
+      if (needsRender(attrs, visibleRect)) {
+        const cellStyle = {
+          position: 'absolute',
+          left: attrs.x,
+          top: attrs.y,
+          width: attrs.width,
+          height: attrs.width,
+        };
 
-          grids.push(
-            <div key={cellKey({ columnIndex, rowIndex })} style={cellStyle}>
-              {renderCell({ columnIndex, rowIndex })}
-            </div>
-          );
-        }
+        grids.push(
+          <div key={itemKey(attrs)} style={cellStyle}>
+            {renderItem(attrs)}
+          </div>
+        );
       }
     }
 
@@ -127,66 +145,171 @@ export default class GridContent extends React.PureComponent {
   }
 }
 
-function needsRender(origin, size, vOrigin, vSize, name) {
+function needsRender(cellRect, visibleRect, name) {
   if (name) {
-    const dOrigin = origin - vOrigin;
+    const dx = cellRect.x - visibleRect.x;
 
-    return -0.25 * vSize < dOrigin + size && dOrigin < 1.25 * vSize;
+    return (
+      -0.25 * visibleRect.width < dx + cellRect.width &&
+      dx < 1.25 * visibleRect.width
+    );
   }
 
   return (
-    needsRender(origin.x, size.width, vOrigin.x, vSize.width, 'x') &&
-    needsRender(origin.y, size.height, vOrigin.y, vSize.height, 'y')
+    needsRender(
+      { x: cellRect.x, width: cellRect.width },
+      { x: visibleRect.x, width: visibleRect.width },
+      'x'
+    ) &&
+    needsRender(
+      { x: cellRect.y, width: cellRect.height },
+      { x: visibleRect.y, width: visibleRect.height },
+      'y'
+    )
   );
 }
 
-function calculateDerivedState(
+function calculateIndexForLayoutAttrs(index, count, direction) {
+  if (!direction) {
+    return index.column + index.row * count.column;
+  }
+
+  if (direction === 'vertical') {
+    return calculateIndexForLayoutAttrs(
+      {
+        row: index.row,
+        column: index.column,
+      },
+      {
+        row: count.row,
+        column: count.column,
+      }
+    );
+  } else if (direction === 'horizontal') {
+    return calculateIndexForLayoutAttrs(
+      {
+        row: index.column,
+        column: index.row,
+      },
+      {
+        row: count.column,
+        column: count.row,
+      }
+    );
+  }
+}
+
+function calculateLayout(
+  size,
+  itemSize,
+  lineSpacing,
+  inneritemSpacing,
   count,
-  sizeFn,
-  sizeHashFn,
-  originList,
-  sizeDict
+  direction
 ) {
-  let shouldUpdateOriginList = originList.length !== count;
-  let nextOriginList = [];
-  let nextSize = 0;
-  let nextSizeDict;
-  let nextState = {};
+  if (!direction) {
+    let sizeWidth = size.width;
+    let sizeHeight = 0;
+    let columnCount = 0;
+    let rowCount = 0;
+    const layoutAttrs = [];
 
-  for (let index = 0; index < count; index++) {
-    let size;
-
-    if (typeof sizeFn === 'number') {
-      size = sizeFn;
+    if (sizeWidth < 0) {
+      sizeWidth =
+        count * itemSize.width +
+        (count <= 1 ? 0 : (count - 1) * inneritemSpacing);
+      columnCount = count;
     } else {
-      const sizeHash = sizeHashFn({ index });
-      size = sizeDict[sizeHash];
-
-      if (size === undefined) {
-        size = sizeFn({ index });
-
-        if (!nextSizeDict) {
-          nextSizeDict = { ...sizeDict };
-        }
-        nextSizeDict[sizeHash] = size;
+      if (itemSize.width === 0 && lineSpacing === 0) {
+        columnCount = count;
+      } else {
+        columnCount =
+          1 +
+          (sizeWidth < itemSize.width
+            ? 0
+            : Math.floor(
+                (sizeWidth - itemSize.width) /
+                  (itemSize.width + inneritemSpacing)
+              ));
       }
     }
 
-    nextSize += size;
-
-    if (originList[index] !== nextSize) {
-      shouldUpdateOriginList = true;
+    if (columnCount > 0) {
+      rowCount = Math.ceil(count / columnCount);
     }
-    nextOriginList[index] = nextSize;
+
+    if (rowCount > 0) {
+      for (let rowIndex = 0; rowIndex < rowCount; rowIndex++) {
+        if (rowIndex > 0) {
+          sizeHeight += lineSpacing + itemSize.height;
+        }
+
+        for (let columnIndex = 0; columnIndex < columnCount; columnIndex++) {
+          const index = columnIndex + rowIndex * columnCount;
+
+          if (index < count) {
+            layoutAttrs.push({
+              x: Math.round(
+                columnIndex * ((sizeWidth - itemSize.width) / (columnCount - 1))
+              ),
+              y: sizeHeight,
+              width: itemSize.width,
+              height: itemSize.height,
+              rowIndex,
+              columnIndex,
+            });
+          }
+        }
+      }
+
+      sizeHeight += itemSize.height;
+    }
+
+    return {
+      rowCount,
+      columnCount,
+      sizeWidth,
+      sizeHeight,
+      layoutAttrs,
+    };
   }
 
-  if (shouldUpdateOriginList) {
-    nextState.originList = nextOriginList;
-  }
-  if (nextSizeDict) {
-    nextState.sizeDict = nextSizeDict;
-  }
-  nextState.size = nextSize;
+  if (direction === 'vertical') {
+    const layout = calculateLayout(
+      { width: size.width, height: size.height },
+      { width: itemSize.width, height: itemSize.height },
+      lineSpacing,
+      inneritemSpacing,
+      count
+    );
 
-  return nextState;
+    return {
+      sizeWidth: layout.sizeWidth,
+      sizeHeight: layout.sizeHeight,
+      rowCount: layout.rowCount,
+      columnCount: layout.columnCount,
+      layoutAttrs: layout.layoutAttrs,
+    };
+  } else if (direction === 'horizontal') {
+    const layout = calculateLayout(
+      { width: size.height, height: size.width },
+      { width: itemSize.height, height: itemSize.width },
+      lineSpacing,
+      inneritemSpacing,
+      count
+    );
+
+    return {
+      sizeWidth: layout.sizeHeight,
+      sizeHeight: layout.sizeWidth,
+      rowCount: layout.columnCount,
+      columnCount: layout.rowCount,
+      layoutAttrs: layout.layoutAttrs.map(attrs => ({
+        x: attrs.y,
+        y: attrs.x,
+        width: attrs.height,
+        height: attrs.width,
+      })),
+    };
+  }
 }
