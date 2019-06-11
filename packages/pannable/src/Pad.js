8aa8859c73cd0170bf83dbfeba90f29559ae19e3
朱德明 <jamingming@163.com>
2019-06-11 15:01:00 +0800

    no message

--- packages/pannable/src/Pad.js
+++ packages/pannable/src/Pad.js
@@ -1,53 +1,527 @@
-import React from 'react';
-import { usePad, defaultPadProps } from './usePad';
+import React, {
+  isValidElement,
+  cloneElement,
+  useState,
+  useEffect,
+  useCallback,
+  useRef,
+  useMemo,
+  useReducer,
+} from 'react';
+import Pannable from './Pannable';
+import PadContext from './PadContext';
+import GeneralContent from './GeneralContent';
+import StyleSheet from './utils/StyleSheet';
+import {
+  requestAnimationFrame,
+  cancelAnimationFrame,
+} from './utils/animationFrame';
+import {
+  getAdjustedContentVelocity,
+  getAdjustedContentOffset,
+  getAdjustedBounceOffset,
+  getDecelerationEndOffset,
+  shouldDragStart,
+  createDeceleration,
+  calculateDeceleration,
+  calculateRectOffset,
+} from './utils/motion';
 
-function Pad({
-  width,
-  height,
-  pagingEnabled,
-  directionalLockEnabled,
-  alwaysBounceX,
-  alwaysBounceY,
-  onScroll,
-  onDragStart,
-  onDragEnd,
-  onDecelerationStart,
-  onDecelerationEnd,
-  onContentResize,
-  enabled,
-  shouldStart,
-  onStart,
-  onMove,
-  onEnd,
-  onCancel,
-  ...props
-}) {
-  const [padProps] = usePad({
-    width,
-    height,
-    pagingEnabled,
-    directionalLockEnabled,
+const defaultPannableProps = Pannable.defaultProps;
+
+const DECELERATION_RATE_STRONG = 0.025;
+const DECELERATION_RATE_WEAK = 0.0025;
+
+const initialState = {
+  contentOffset: { x: 0, y: 0 },
+  contentVelocity: { x: 0, y: 0 },
+  drag: null,
+  deceleration: null,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'render':
+      return renderReducer(state, action);
+    case 'dragStart':
+      return dragStartReducer(state, action);
+    case 'dragMove':
+      return dragMoveReducer(state, action);
+    case 'dragEnd':
+      return dragEndReducer(state, action);
+    case 'dragCancel':
+      return dragCancelReducer(state, action);
+    case 'decelerate':
+      return decelerateReducer(state, action);
+    default:
+      return state;
+  }
+}
+
+function renderReducer(state, action) {
+  const { contentOffset, contentVelocity, drag, deceleration } = state;
+  const { size, contentSize, pagingEnabled } = action;
+
+  const decelerationRate = DECELERATION_RATE_STRONG;
+
+  if (deceleration) {
+    let decelerationEndOffset = deceleration.endOffset;
+
+    if (
+      decelerationEndOffset !==
+        getAdjustedContentOffset(
+          decelerationEndOffset,
+          size,
+          contentSize,
+          pagingEnabled,
+          true
+        ) &&
+      contentOffset !==
+        getAdjustedContentOffset(
+          contentOffset,
+          size,
+          contentSize,
+          pagingEnabled,
+          true
+        )
+    ) {
+      let nextContentVelocity = contentVelocity;
+
+      if (deceleration.rate !== decelerationRate) {
+        nextContentVelocity = getAdjustedContentVelocity(
+          nextContentVelocity,
+          size,
+          decelerationRate
+        );
+        decelerationEndOffset = getDecelerationEndOffset(
+          contentOffset,
+          nextContentVelocity,
+          size,
+          pagingEnabled,
+          decelerationRate
+        );
+      }
+
+      decelerationEndOffset = getAdjustedContentOffset(
+        decelerationEndOffset,
+        size,
+        contentSize,
+        pagingEnabled,
+        true
+      );
+
+      return {
+        contentOffset: { ...contentOffset },
+        contentVelocity: nextContentVelocity,
+        drag: null,
+        deceleration: createDeceleration(
+          contentOffset,
+          nextContentVelocity,
+          decelerationEndOffset,
+          decelerationRate
+        ),
+      };
+    }
+  } else if (!drag) {
+    const adjustedContentOffset = getAdjustedContentOffset(
+      contentOffset,
+      size,
+      contentSize,
+      pagingEnabled
+    );
+
+    if (contentOffset !== adjustedContentOffset) {
+      const decelerationEndOffset = getDecelerationEndOffset(
+        adjustedContentOffset,
+        { x: 0, y: 0 },
+        size,
+        pagingEnabled,
+        decelerationRate
+      );
+
+      return {
+        contentOffset: { ...contentOffset },
+        contentVelocity,
+        drag: null,
+        deceleration: createDeceleration(
+          contentOffset,
+          contentVelocity,
+          decelerationEndOffset,
+          decelerationRate
+        ),
+      };
+    }
+  }
+
+  return state;
+}
+
+function dragStartReducer(state, action) {
+  const { contentOffset } = state;
+  const { directionalLockEnabled, velocity } = action;
+
+  const dragDirection = { x: 1, y: 1 };
+
+  if (directionalLockEnabled) {
+    if (Math.abs(velocity.x) > Math.abs(velocity.y)) {
+      dragDirection.y = 0;
+    } else {
+      dragDirection.x = 0;
+    }
+  }
+
+  const nextContentVelocity = {
+    x: dragDirection.x * velocity.x,
+    y: dragDirection.y * velocity.y,
+  };
+
+  return {
+    contentOffset: { ...contentOffset },
+    contentVelocity: nextContentVelocity,
+    drag: { direction: dragDirection, startOffset: contentOffset },
+    deceleration: null,
+  };
+}
+
+function dragMoveReducer(state, action) {
+  const { contentOffset, drag } = state;
+  const {
     alwaysBounceX,
     alwaysBounceY,
-    onScroll,
-    onDragStart,
-    onDragEnd,
-    onDecelerationStart,
-    onDecelerationEnd,
-    onContentResize,
-    enabled,
-    shouldStart,
-    onStart,
-    onMove,
-    onEnd,
-    onCancel,
+    size,
+    contentSize,
+    translation,
+    interval,
+  } = action;
+
+  const nextContentOffset = getAdjustedBounceOffset(
+    {
+      x: drag.startOffset.x + drag.direction.x * translation.x,
+      y: drag.startOffset.y + drag.direction.y * translation.y,
+    },
+    { x: alwaysBounceX, y: alwaysBounceY },
+    size,
+    contentSize
+  );
+  const nextContentVelocity = {
+    x: (nextContentOffset.x - contentOffset.x) / interval,
+    y: (nextContentOffset.y - contentOffset.y) / interval,
+  };
+
+  return {
+    contentOffset: nextContentOffset,
+    contentVelocity: nextContentVelocity,
+    drag,
+    deceleration: null,
+  };
+}
+
+function dragEndReducer(state, action) {
+  const { contentOffset, contentVelocity } = state;
+  const { pagingEnabled, size } = action;
+
+  let decelerationRate = DECELERATION_RATE_WEAK;
+  let nextContentVelocity = contentVelocity;
+
+  if (pagingEnabled) {
+    decelerationRate = DECELERATION_RATE_STRONG;
+    nextContentVelocity = getAdjustedContentVelocity(
+      nextContentVelocity,
+      size,
+      decelerationRate
+    );
+  }
+
+  const decelerationEndOffset = getDecelerationEndOffset(
+    contentOffset,
+    nextContentVelocity,
+    size,
+    pagingEnabled,
+    decelerationRate
+  );
+
+  return {
+    contentOffset: { ...contentOffset },
+    contentVelocity: nextContentVelocity,
+    drag: null,
+    deceleration: createDeceleration(
+      contentOffset,
+      nextContentVelocity,
+      decelerationEndOffset,
+      decelerationRate
+    ),
+  };
+}
+
+function dragCancelReducer(state, action) {
+  const { contentOffset, contentVelocity, drag } = state;
+  const { pagingEnabled, size } = action;
+
+  const decelerationRate = DECELERATION_RATE_STRONG;
+  const decelerationEndOffset = getDecelerationEndOffset(
+    drag.startOffset,
+    { x: 0, y: 0 },
+    size,
+    pagingEnabled,
+    decelerationRate
+  );
+
+  return {
+    contentOffset: { ...contentOffset },
+    contentVelocity,
+    drag: null,
+    deceleration: createDeceleration(
+      contentOffset,
+      contentVelocity,
+      decelerationEndOffset,
+      decelerationRate
+    ),
+  };
+}
+
+function decelerateReducer(state, action) {
+  const { deceleration } = state;
+  const { now: moveTime } = action;
+
+  if (!deceleration) {
+    return state;
+  }
+  if (deceleration.startTime + deceleration.duration <= moveTime) {
+    return {
+      contentOffset: deceleration.endOffset,
+      contentVelocity: { x: 0, y: 0 },
+      drag: null,
+      deceleration: null,
+    };
+  }
+
+  const { xOffset, yOffset, xVelocity, yVelocity } = calculateDeceleration(
+    deceleration,
+    moveTime
+  );
+
+  return {
+    contentOffset: { x: xOffset, y: yOffset },
+    contentVelocity: { x: xVelocity, y: yVelocity },
+    drag: null,
+    deceleration,
+  };
+}
+
+const defaultPadProps = {
+  width: 0,
+  height: 0,
+  pagingEnabled: false,
+  directionalLockEnabled: false,
+  alwaysBounceX: true,
+  alwaysBounceY: true,
+  onScroll: () => {},
+  onDragStart: () => {},
+  onDragEnd: () => {},
+  onDecelerationStart: () => {},
+  onDecelerationEnd: () => {},
+  onContentResize: () => {},
+};
+
+function Pad({
+  width = defaultPadProps.width,
+  height = defaultPadProps.height,
+  pagingEnabled = defaultPadProps.pagingEnabled,
+  directionalLockEnabled = defaultPadProps.directionalLockEnabled,
+  alwaysBounceX = defaultPadProps.alwaysBounceX,
+  alwaysBounceY = defaultPadProps.alwaysBounceY,
+  onScroll = defaultPadProps.onScroll,
+  onDragStart = defaultPadProps.onDragStart,
+  onDragEnd = defaultPadProps.onDragEnd,
+  onDecelerationStart = defaultPadProps.onDecelerationStart,
+  onDecelerationEnd = defaultPadProps.onDecelerationEnd,
+  onContentResize = defaultPadProps.onContentResize,
+  ...pannableProps
+}) {
+  const size = useMemo(() => ({ width, height }), [width, height]);
+  const [contentSize, setContentSize] = useState({ width: 0, height: 0 });
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const eventRef = useRef({ state, contentSize });
+
+  const {
+    shouldStart = defaultPannableProps.shouldStart,
+    onStart = defaultPannableProps.onStart,
+    onMove = defaultPannableProps.onMove,
+    onEnd = defaultPannableProps.onEnd,
+    onCancel = defaultPannableProps.onCancel,
+  } = pannableProps;
+
+  eventRef.current.shouldStart = shouldStart;
+  eventRef.current.onStart = onStart;
+  eventRef.current.onMove = onMove;
+  eventRef.current.onEnd = onEnd;
+  eventRef.current.onCancel = onCancel;
+
+  const resizeContent = useCallback(size => setContentSize(size), []);
+  const shouldPannableStart = useCallback(
+    evt => {
+      const { velocity } = evt;
+
+      if (
+        directionalLockEnabled &&
+        !shouldDragStart(velocity, size, contentSize)
+      ) {
+        return false;
+      }
+
+      return eventRef.current.shouldStart(evt);
+    },
+    [directionalLockEnabled, size, contentSize]
+  );
+
+  const onPannableStart = useCallback(
+    evt => {
+      dispatch({
+        type: 'dragStart',
+        directionalLockEnabled,
+        velocity: evt.velocity,
+      });
+      eventRef.current.onStart(evt);
+    },
+    [directionalLockEnabled]
+  );
+
+  const onPannableMove = useCallback(
+    evt => {
+      dispatch({
+        type: 'dragMove',
+        alwaysBounceX,
+        alwaysBounceY,
+        size,
+        contentSize,
+        translation: evt.translation,
+        interval: evt.interval,
+      });
+      eventRef.current.onMove(evt);
+    },
+    [alwaysBounceX, alwaysBounceY, size, contentSize]
+  );
+
+  const onPannableEnd = useCallback(
+    evt => {
+      dispatch({ type: 'dragEnd', pagingEnabled, size });
+      eventRef.current.onEnd(evt);
+    },
+    [pagingEnabled, size]
+  );
+
+  const onPannableCancel = useCallback(
+    evt => {
+      dispatch({ type: 'dragCancel', pagingEnabled, size });
+      eventRef.current.onCancel(evt);
+    },
+    [pagingEnabled, size]
+  );
+
+  const decelerate = useCallback(() => {
+    dispatch({ type: 'decelerate', now: new Date().getTime() });
+  }, []);
+
+  useEffect(() => {
+    if (state.deceleration) {
+      const timer = requestAnimationFrame(() => decelerate());
+
+      return () => cancelAnimationFrame(timer);
+    }
+  }, [state, decelerate]);
+
+  useMemo(() => {
+    if (!state.drag) {
+      dispatch({ type: 'render', size, contentSize, pagingEnabled });
+    }
+  }, [size, contentSize, pagingEnabled, state]);
+
+  const elemStyle = {
+    overflow: 'hidden',
+    position: 'relative',
+    width: size.width,
+    height: size.height,
+  };
+
+  const contentStyle = StyleSheet.create({
+    position: 'relative',
+    width: contentSize.width,
+    height: contentSize.height,
+    transformTranslate: [state.contentOffset.x, state.contentOffset.y],
+    willChange: 'transform',
   });
 
-  props.ref = padProps.ref;
-  props.style = { ...padProps.style, ...props.style };
-  props.children = padProps.render(props.children);
+  const visibleRect = {
+    x: -state.contentOffset.x,
+    y: -state.contentOffset.y,
+    width: size.width,
+    height: size.height,
+  };
 
-  return <div {...props} />;
+  pannableProps.shouldStart = shouldPannableStart;
+  pannableProps.onStart = onPannableStart;
+  pannableProps.onMove = onPannableMove;
+  pannableProps.onEnd = onPannableEnd;
+  pannableProps.onCancel = onPannableCancel;
+
+  useEffect(() => {
+    const { state: prevState, contentSize: prevContentSize } = eventRef.current;
+    const output = {
+      size,
+      contentSize,
+      contentOffset: state.contentOffset,
+      contentVelocity: state.contentVelocity,
+      dragging: !!state.drag,
+      decelerating: !!state.deceleration,
+    };
+
+    eventRef.current.state = state;
+    eventRef.current.contentSize = contentSize;
+
+    if (contentSize !== prevContentSize) {
+      onContentResize(contentSize);
+    }
+    if (state.contentOffset !== prevState.contentOffset) {
+      onScroll(output);
+    }
+    if (state.drag !== prevState.drag) {
+      if (!prevState.drag) {
+        onDragStart(output);
+      } else if (!state.drag) {
+        onDragEnd(output);
+      }
+    }
+    if (state.deceleration !== prevState.deceleration) {
+      if (!prevState.deceleration) {
+        onDecelerationStart(output);
+      } else if (!state.deceleration) {
+        onDecelerationEnd(output);
+      }
+    }
+  });
+
+  pannableProps.style = { ...elemStyle, ...pannableProps.style };
+
+  let element = pannableProps.children;
+
+  if (isValidElement(element) && element.type.PadContent) {
+    element = cloneElement(element, {
+      style: { ...contentStyle, ...element.props.style },
+      ref: element.ref,
+    });
+  } else {
+    element = <GeneralContent style={contentStyle}>{element}</GeneralContent>;
+  }
+
+  return (
+    <Pannable {...pannableProps}>
+      <PadContext.Provider
+        value={{ visibleRect, onContentResize: resizeContent }}
+      >
+        {element}
+      </PadContext.Provider>
+    </Pannable>
+  );
 }
 
 Pad.defaultProps = defaultPadProps;
--- packages/pannable/src/Pannable.js
+++ packages/pannable/src/Pannable.js
@@ -1,26 +1,328 @@
-import React, { useEffect } from 'react';
-import { usePannable, defaultPannableProps } from './usePannable';
+import React from 'react';
+import { useEffect, useRef, useMemo, useReducer } from 'react';
+
+const MIN_DISTANCE = 0;
+
+/* eslint no-restricted-globals:"off" */
+
+let root;
+
+if (typeof self !== 'undefined') {
+  root = self;
+} else if (typeof window !== 'undefined') {
+  root = window;
+} else if (typeof global !== 'undefined') {
+  root = global;
+} else {
+  root = {};
+}
+
+const initialState = {
+  target: null,
+  startPoint: null,
+  movePoint: null,
+  moveTime: null,
+  translation: null,
+  velocity: null,
+  interval: null,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'disable':
+    case 'end':
+      return disableReducer(state, action);
+    case 'track':
+      return trackReducer(state, action);
+    case 'move':
+      return moveReducer(state, action);
+    default:
+      return state;
+  }
+}
+
+function disableReducer(state, action) {
+  const { target } = state;
+
+  return target ? initialState : state;
+}
+
+function trackReducer(state, action) {
+  return {
+    target: action.target,
+    startPoint: action.point,
+    movePoint: action.point,
+    moveTime: action.now,
+    translation: null,
+    velocity: null,
+    interval: null,
+  };
+}
+
+function moveReducer(state, action) {
+  const { target, startPoint, movePoint, moveTime, translation } = state;
+  const { point: nextMovePoint, now: nextMoveTime, shouldStart } = action;
+
+  if (!target) {
+    return state;
+  }
+
+  const nextInterval = nextMoveTime - moveTime;
+  const nextTranslation = {
+    x: nextMovePoint.x - startPoint.x,
+    y: nextMovePoint.y - startPoint.y,
+  };
+  const nextVelocity = {
+    x: (nextMovePoint.x - movePoint.x) / nextInterval,
+    y: (nextMovePoint.y - movePoint.y) / nextInterval,
+  };
+
+  if (translation) {
+    return {
+      target,
+      startPoint,
+      movePoint: nextMovePoint,
+      moveTime: nextMoveTime,
+      translation: nextTranslation,
+      velocity: nextVelocity,
+      interval: nextInterval,
+    };
+  }
+
+  const dist = Math.sqrt(
+    Math.pow(nextTranslation.x, 2) + Math.pow(nextTranslation.y, 2)
+  );
+
+  if (dist <= MIN_DISTANCE) {
+    return {
+      target,
+      startPoint,
+      movePoint: nextMovePoint,
+      moveTime: nextMoveTime,
+      translation: null,
+      velocity: null,
+      interval: null,
+    };
+  }
+
+  if (
+    !shouldStart({
+      target,
+      translation: nextTranslation,
+      velocity: nextVelocity,
+      interval: nextInterval,
+    })
+  ) {
+    return initialState;
+  }
+
+  return {
+    target,
+    startPoint: nextMovePoint,
+    movePoint: nextMovePoint,
+    moveTime: nextMoveTime,
+    translation: { x: 0, y: 0 },
+    velocity: nextVelocity,
+    interval: nextInterval,
+  };
+}
+
+const defaultPannableProps = {
+  enabled: true,
+  shouldStart: () => true,
+  onStart: () => {},
+  onMove: () => {},
+  onEnd: () => {},
+  onCancel: () => {},
+};
 
 function Pannable({
-  enabled,
-  shouldStart,
-  onStart,
-  onMove,
-  onEnd,
-  onCancel,
+  enabled = defaultPannableProps.enabled,
+  shouldStart = defaultPannableProps.shouldStart,
+  onStart = defaultPannableProps.onStart,
+  onMove = defaultPannableProps.onMove,
+  onEnd = defaultPannableProps.onEnd,
+  onCancel = defaultPannableProps.onCancel,
   ...props
 }) {
-  const [pannableProps] = usePannable({
-    enabled,
-    shouldStart,
-    onStart,
-    onMove,
-    onEnd,
-    onCancel,
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const elemRef = useRef(null);
+  const eventRef = useRef({ state, touchSupported: false });
+
+  eventRef.current.shouldStart = shouldStart;
+  eventRef.current.onStart = onStart;
+  eventRef.current.onMove = onMove;
+  eventRef.current.onEnd = onEnd;
+  eventRef.current.onCancel = onCancel;
+
+  useEffect(() => {
+    function track(target, point) {
+      dispatch({ type: 'track', target, point, now: new Date().getTime() });
+    }
+
+    function onTouchStart(evt) {
+      eventRef.current.touchSupported = true;
+
+      if (evt.touches && evt.touches.length === 1) {
+        const touchEvent = evt.touches[0];
+
+        track(touchEvent.target, { x: touchEvent.pageX, y: touchEvent.pageY });
+      }
+    }
+
+    function onTouchMove(evt) {
+      if (eventRef.current.state.translation) {
+        evt.preventDefault();
+      }
+    }
+
+    function onMouseDown(evt) {
+      if (eventRef.current.touchSupported) {
+        return;
+      }
+
+      track(evt.target, { x: evt.pageX, y: evt.pageY });
+    }
+
+    if (enabled) {
+      const elemNode = elemRef.current;
+
+      if (!elemNode.addEventListener) {
+        return;
+      }
+
+      elemNode.addEventListener('touchstart', onTouchStart, false);
+      elemNode.addEventListener('touchmove', onTouchMove, false);
+      elemNode.addEventListener('mousedown', onMouseDown, false);
+
+      return () => {
+        elemNode.removeEventListener('touchstart', onTouchStart, false);
+        elemNode.removeEventListener('touchmove', onTouchMove, false);
+        elemNode.removeEventListener('mousedown', onMouseDown, false);
+      };
+    }
+  }, [enabled]);
+
+  useEffect(() => {
+    function move(point) {
+      dispatch({
+        type: 'move',
+        point,
+        now: new Date().getTime(),
+        shouldStart: eventRef.current.shouldStart,
+      });
+    }
+
+    function end() {
+      dispatch({ type: 'end' });
+    }
+
+    function onTargetTouchMove(evt) {
+      const touchEvent = evt.touches[0];
+
+      if (eventRef.current.state.translation) {
+        evt.preventDefault();
+      }
+
+      move({ x: touchEvent.pageX, y: touchEvent.pageY });
+    }
+
+    function onTargetTouchEnd(evt) {
+      if (eventRef.current.state.translation) {
+        evt.preventDefault();
+      }
+
+      end();
+    }
+
+    function onRootMouseMove(evt) {
+      evt.preventDefault();
+
+      move({ x: evt.pageX, y: evt.pageY });
+    }
+
+    function onRootMouseUp(evt) {
+      evt.preventDefault();
+
+      end();
+    }
+
+    const target = state.target;
+
+    if (!target) {
+      return;
+    }
+
+    if (eventRef.current.touchSupported) {
+      target.addEventListener('touchmove', onTargetTouchMove, false);
+      target.addEventListener('touchend', onTargetTouchEnd, false);
+      target.addEventListener('touchcancel', onTargetTouchEnd, false);
+
+      return () => {
+        target.removeEventListener('touchmove', onTargetTouchMove, false);
+        target.removeEventListener('touchend', onTargetTouchEnd, false);
+        target.removeEventListener('touchcancel', onTargetTouchEnd, false);
+      };
+    } else {
+      root.addEventListener('mousemove', onRootMouseMove, false);
+      root.addEventListener('mouseup', onRootMouseUp, false);
+
+      return () => {
+        root.removeEventListener('mousemove', onRootMouseMove, false);
+        root.removeEventListener('mouseup', onRootMouseUp, false);
+      };
+    }
+  }, [state.target]);
+
+  useEffect(() => {
+    const {
+      state: prevState,
+      onStart,
+      onMove,
+      onEnd,
+      onCancel,
+    } = eventRef.current;
+    const output = {
+      target: state.target,
+      translation: state.translation,
+      velocity: state.velocity,
+      interval: state.interval,
+    };
+
+    eventRef.current.state = state;
+
+    if (state.translation !== prevState.translation) {
+      if (state.translation) {
+        if (prevState.translation) {
+          onMove(output);
+        } else {
+          onStart(output);
+        }
+      } else if (prevState.translation) {
+        if (enabled) {
+          onEnd(output);
+        } else {
+          onCancel(output);
+        }
+      }
+    }
   });
 
-  props.ref = pannableProps.ref;
-  props.style = { ...pannableProps.style, ...props.style };
+  useMemo(() => {
+    if (!enabled) {
+      dispatch({ type: 'disable' });
+    }
+  }, [enabled]);
+
+  const elemStyle = {};
+
+  if (state.translation) {
+    elemStyle.touchAction = 'none';
+    elemStyle.pointerEvents = 'none';
+  }
+
+  props.style = { ...elemStyle, ...props.style };
+  props.ref = elemRef;
 
   return <div {...props} />;
 }
