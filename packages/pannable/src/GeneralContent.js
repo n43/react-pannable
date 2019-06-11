dda14f5be32eeee1f53ed15c86a81e1d9bf68d7c
朱德明 <jamingming@163.com>
2019-06-11 15:51:47 +0800

    no message

--- packages/pannable/src/GeneralContent.js
+++ packages/pannable/src/GeneralContent.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import {
-  useGeneralContent,
-  defaultGeneralContentProps,
-} from './useGeneralContent';
+import React, { useEffect, useRef } from 'react';
+import ItemContent from './ItemContent';
+import resizeDetector from './utils/resizeDetector';
 
-function GeneralContent({ width, height, ...props }) {
-  const [contentProps] = useGeneralContent(props);
+const defaultGeneralContentProps = ItemContent.defaultProps;
 
-  props.style = { ...contentProps.style, ...props.style };
-  props.children = contentProps.render(props.children);
+function GeneralContent({
+  width = defaultGeneralContentProps.width,
+  height = defaultGeneralContentProps.height,
+  ...props
+}) {
+  const elemRef = useRef(null);
 
-  return <div {...props} />;
+  useEffect(() => {
+    if (typeof width === 'number' && typeof height === 'number') {
+      return;
+    }
+
+    const resizeNode = elemRef.current.getResizeNode();
+
+    resizeDetector.listenTo(resizeNode, () => elemRef.current.calculateSize());
+
+    return () => resizeDetector.uninstall(resizeNode);
+  }, [width, height]);
+
+  return <ItemContent {...props} ref={elemRef} />;
 }
 GeneralContent.defaultProps = defaultGeneralContentProps;
 GeneralContent.PadContent = true;
--- packages/pannable/src/ItemContent.js
+++ packages/pannable/src/ItemContent.js
@@ -1,14 +1,91 @@
-import React from 'react';
-import { useItemContent, defaultItemContentProps } from './useItemContent';
+import React, {
+  forwardRef,
+  useState,
+  useEffect,
+  useCallback,
+  useRef,
+  useContext,
+  useMemo,
+  useImperativeHandle,
+} from 'react';
+import PadContext from './PadContext';
+import { getElementSize } from './utils/sizeGetter';
+import { isEqualToSize } from './utils/geometry';
 
-function ItemContent({ width, height, ...props }) {
-  const [contentProps] = useItemContent({ width, height });
+const defaultItemContentProps = {
+  width: null,
+  height: null,
+};
 
-  props.style = { ...contentProps.style, ...props.style };
-  props.children = contentProps.render(props.children);
+const ItemContent = forwardRef(function(
+  {
+    width = defaultItemContentProps.width,
+    height = defaultItemContentProps.height,
+    ...props
+  },
+  ref
+) {
+  const context = useContext(PadContext);
+  const [size, setSize] = useState(null);
+  const resizeRef = useRef(null);
+  const eventRef = useRef({ size });
 
-  return <div {...props} />;
-}
+  const resizeContent = useCallback(() => {}, []);
+  const getResizeNode = useCallback(() => resizeRef.current, []);
+  const calculateSize = useCallback(() => {
+    const nextSize = getElementSize(resizeRef.current);
+
+    setSize(nextSize);
+  }, []);
+
+  useEffect(() => {
+    const { size: prevSize } = eventRef.current;
+
+    eventRef.current.size = size;
+
+    if (!isEqualToSize(size, prevSize)) {
+      if (size) {
+        context.onContentResize(size);
+      } else {
+        calculateSize();
+      }
+    }
+  });
+
+  useMemo(() => {
+    if (typeof width === 'number' && typeof height === 'number') {
+      setSize({ width, height });
+    }
+  }, [width, height]);
+
+  const elemStyle = { position: 'relative' };
+  const resizeStyle = { position: 'absolute' };
+
+  if (typeof width === 'number') {
+    resizeStyle.width = width;
+  }
+  if (typeof height === 'number') {
+    resizeStyle.height = height;
+  }
+  if (size) {
+    elemStyle.width = size.width;
+    elemStyle.height = size.height;
+  }
+
+  props.style = { ...elemStyle, ...props.style };
+
+  useImperativeHandle(ref, () => ({ getResizeNode, calculateSize }));
+
+  return (
+    <PadContext.Provider value={{ ...context, onContentResize: resizeContent }}>
+      <div {...props}>
+        <div ref={resizeRef} style={resizeStyle}>
+          {props.children}
+        </div>
+      </div>
+    </PadContext.Provider>
+  );
+});
 
 ItemContent.defaultProps = defaultItemContentProps;
 ItemContent.PadContent = true;
