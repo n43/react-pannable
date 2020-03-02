c8204620c98a1d761359b5d9095785ab3c2d1e1b
ming <jamingming@163.com>
2020-03-02 15:23:32 +0800

    更新依赖

--- packages/pannable/rollup.config.js
+++ packages/pannable/rollup.config.js
@@ -1,7 +1,7 @@
-import nodeResolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import babel from 'rollup-plugin-babel';
-import replace from 'rollup-plugin-replace';
+import replace from '@rollup/plugin-replace';
 import { terser } from 'rollup-plugin-terser';
 
 const env = process.env.NODE_ENV;
